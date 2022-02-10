1eee4400406447416234b513e50f1628bb1cd019
AdityaNambidi <aditya200513@gmail.com>
2022-02-10 21:08:30 +0400

    Adding arduino code

---
+++ Sensor_Arduino/PIR_Sensor.ino
@@ -0,0 +1,79 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+WiFiClient wifiClient;
+const int httpsPort = 443;
+const char* host = "www.smartswitch.ga";
+const char* key = "957eb0aba638ea9b0bc64fa9004237d3d63d50e1";
+
+String id = "5576";
+
+int pirSensor = 2;
+
+float waitTime = 10000;
+float increment = 210;
+
+void setup() {
+  pinMode(pirSensor, INPUT);
+  pinMode(0, OUTPUT);
+  
+  Serial.begin(9600);
+
+
+  WiFi.begin("Free Wifi", "1223334444@"); 
+  
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+
+  }
+
+  Serial.println("connected to wifey");
+  
+  delay(500);
+}
+
+void loop() {
+
+  delay(10000);
+
+  float sensorValue = digitalRead(pirSensor);
+
+  if (sensorValue == HIGH) {
+    
+    digitalWrite(0, HIGH);
+    delay(5000);
+  
+  } else {
+    digitalWrite(0, LOW);
+    notify(id ); 
+  }
+}
+
+
+void notify(String id) {
+
+  WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
+
+
+  //Serial.printf("Using fingerprint '%s'\n", key);
+  httpsClient.setFingerprint(key);
+  
+  //Serial.print("HTTPS Connecting");
+  int r=0; //retry counter
+  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      //Serial.print(".");
+      r++;
+  }
+  
+  String Link = "/switch/" + id + "/statusUpdate/x";
+
+//  Serial.print("requesting URL: ");
+ // Serial.println(host+Link);
+
+  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +               
+               "Connection: close\r\n\r\n");
+}
---
+++ smartswitch_Arduino/smartswitch.ino
@@ -0,0 +1,121 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+WiFiClient wifiClient;
+const int httpsPort = 443;
+const String host = "www.smartswitch.ga";
+const char* key = "957eb0aba638ea9b0bc64fa9004237d3d63d50e1";
+
+int b = 4;
+int f = 0;
+int s = 2;
+
+String id = "5576";
+
+String state = "";
+
+void setup() {
+
+  pinMode(f, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(s, OUTPUT);
+
+  analogWrite(s, 255);
+
+  Serial.begin(9600);
+  WiFi.begin("Free Wifi", "1223334444@"); 
+  
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+
+  }
+
+  Serial.println("connected to wifey");
+}
+
+int _i = 0;
+
+void loop() { 
+
+    String s = GetData(id, "get");
+
+  Serial.println(s);
+
+    if (_i == 0){
+      state = s;
+      _i++;
+    }
+
+    if (state != s) {
+      if (s == "0"){  // 0 stands for off and 1 stands for on
+        Off();
+      } else if (s == "1"){
+        On();
+      }
+
+      state = s;
+    }
+
+}
+
+void On(){
+  front();
+  delay(500);
+  Stop();
+}
+
+void Off(){
+  back();
+  delay(500);
+  Stop();
+}
+
+void front(){
+  digitalWrite(f, HIGH);
+  digitalWrite(b, LOW);
+}
+
+void back(){
+  digitalWrite(f, LOW);
+  digitalWrite(b, HIGH);}
+
+void Stop(){
+  digitalWrite(f, LOW);
+  digitalWrite(b, LOW);
+}
+
+String GetData(String id, String task){
+
+//  WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
+//
+//
+//  //Serial.printf("Using fingerprint '%s'\n", key);
+//  httpsClient.setFingerprint(key);
+//  
+//  //Serial.print("HTTPS Connecting");
+//  int r=0; //retry counter
+//  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+//      delay(100);
+//      //Serial.print(".");
+//      r++;
+//  }
+//  
+//  String Link = "/switch/" + id + "/" + task + "/x";
+//
+////  Serial.print("requesting URL: ");
+// // Serial.println(host+Link);
+//
+//  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
+//               "Host: " + host + "\r\n" +               
+//               "Connection: close\r\n\r\n");
+//
+//  String line;
+//   
+//  line = httpsClient.readStringUntil('\n');  //Read Line by Line
+//
+//  return line;
+
+
+}
